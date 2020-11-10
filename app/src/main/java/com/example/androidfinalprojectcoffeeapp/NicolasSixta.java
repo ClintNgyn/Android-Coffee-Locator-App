ab8d9245da6b4b86237e81801f4dde6bb8deb9e0
Sixta <nicolassixta@gmail.com>
2020-11-10 18:00:47 -0500

    Nicolas ADDED ACTVIITY

---
+++ app/src/main/java/com/example/androidfinalprojectcoffeeapp/NicolasSixta.java
@@ -0,0 +1,14 @@
+package com.example.androidfinalprojectcoffeeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NicolasSixta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nicolas_sixta);
+    }
+}
