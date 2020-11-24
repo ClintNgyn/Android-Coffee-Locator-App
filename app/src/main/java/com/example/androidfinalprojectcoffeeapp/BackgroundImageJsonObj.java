dc44bdbc7ea53e8dd59309503f4eb01368895a62
Clinton <nguyennoelclinton1@gmail.com>
2020-11-24 18:36:09 -0500

    layout designing and random coffee image api implementation

---
+++ app/src/main/java/com/example/androidfinalprojectcoffeeapp/BackgroundImageJsonObj.java
@@ -0,0 +1,14 @@
+package com.example.androidfinalprojectcoffeeapp;
+
+public class BackgroundImageJsonObj {
+  
+  private String file;
+  
+  public String getFileImageUrl() {
+    return file;
+  }
+  
+  public void setFileImageUri(String file) {
+    this.file = file;
+  }
+}
