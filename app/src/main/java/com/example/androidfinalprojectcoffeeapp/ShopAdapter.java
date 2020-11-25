7d8a1cb54a955d35157c79a2d4d743ae9f7770f3
Clinton <nguyennoelclinton1@gmail.com>
2020-11-24 21:43:57 -0500

    ShopAdapter.java implemented infobtn onlcick

--- app/src/main/java/com/example/androidfinalprojectcoffeeapp/ShopAdapter.java
+++ app/src/main/java/com/example/androidfinalprojectcoffeeapp/ShopAdapter.java
@@ -17,92 +17,97 @@ import java.io.Serializable;
 import java.util.List;
 
 public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ViewHolderOfMapsRecyclerView> {
-    private Context context;
-    private List<ShopJsonObj> shopsList;
-
-    public ShopAdapter(Context context, List<ShopJsonObj> shopsList) {
-        this.context = context;
-        this.shopsList = shopsList;
+  private Context context;
+  private List<ShopJsonObj> shopsList;
+  
+  public ShopAdapter(Context context, List<ShopJsonObj> shopsList) {
+    this.context = context;
+    this.shopsList = shopsList;
+  }
+  
+  @NonNull
+  @Override
+  public ViewHolderOfMapsRecyclerView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    LayoutInflater inflater = LayoutInflater.from(context);
+    View view = inflater.inflate(R.layout.recycler_shops_item, parent, false);
+    return new ViewHolderOfMapsRecyclerView(view);
+  }
+  
+  @Override
+  public void onBindViewHolder(@NonNull ViewHolderOfMapsRecyclerView holder, int position) {
+    ShopJsonObj currShop = shopsList.get(position);
+    
+    // set tvTypeId
+    holder.tvTypeId.setText(currShop.getType());
+    
+    // set tvAddressId
+    holder.tvAddressId.setText(currShop.getAddress());
+    
+    //set init heart image
+    holder.ivFavsId.setImageResource(currShop.isHeartChecked() ? R.drawable.ic_heart_colored : R.drawable.ic_heart_uncolored);
+    
+    // set ivFavsId onclick handler
+    holder.ivFavsId.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        boolean isHeartChecked = currShop.isHeartChecked();
+        
+        // toggle heart
+        holder.ivFavsId.setImageResource(isHeartChecked ? R.drawable.ic_heart_uncolored : R.drawable.ic_heart_colored);
+        
+        // TODO: add or remove from favorite places
+        
+        //set isHeartChecked to !isHeartChecked
+        currShop.setHeartChecked(!isHeartChecked);
+      }
+    });
+    
+    // set ivInfoBtnId onclick handler
+    holder.ivInfoBtnId.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // open ShopDetails Activity
+        openShopDetailsActivity(currShop);
+      }
+    });
+    
+    // set cvCardViewId onclick handler
+    holder.cvCardViewId.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // open ShopDetails Activity
+        openShopDetailsActivity(currShop);
+      }
+    });
+  }
+  
+  private void openShopDetailsActivity(ShopJsonObj currShop) {
+    Intent i = new Intent(context, ShopDetailsActivity.class);
+    i.putExtra("currShop", (Serializable) currShop);
+    context.startActivity(i);
+  }
+  
+  @Override
+  public int getItemCount() {
+    return shopsList.size();
+  }
+  
+  
+  public static class ViewHolderOfMapsRecyclerView extends RecyclerView.ViewHolder {
+    private CardView cvCardViewId;
+    private ImageView ivFavsId, ivInfoBtnId;
+    private TextView tvTypeId, tvAddressId;
+    
+    
+    private ViewHolderOfMapsRecyclerView(@NonNull View itemView) {
+      super(itemView);
+      
+      //link Views
+      cvCardViewId = itemView.findViewById(R.id.cvCardViewId);
+      ivFavsId = itemView.findViewById(R.id.ivFavsId);
+      tvTypeId = itemView.findViewById(R.id.tvTypeId);
+      tvAddressId = itemView.findViewById(R.id.tvAddressId);
+      ivInfoBtnId = itemView.findViewById(R.id.ivInfoBtnId);
     }
-
-    @NonNull
-    @Override
-    public ViewHolderOfMapsRecyclerView onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.recycler_shops_item, parent, false);
-        return new ViewHolderOfMapsRecyclerView(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolderOfMapsRecyclerView holder, int position) {
-        ShopJsonObj currShop = shopsList.get(position);
-
-        // set tvTypeId
-        holder.tvTypeId.setText(currShop.getType());
-
-        // set tvAddressId
-        holder.tvAddressId.setText(currShop.getAddress());
-
-        //set init heart image
-        holder.ivFavsId.setImageResource(currShop.isHeartChecked() ? R.drawable.ic_heart_colored : R.drawable.ic_heart_uncolored);
-
-        // set ivFavsId onclick handler
-        holder.ivFavsId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                boolean isHeartChecked = currShop.isHeartChecked();
-
-                // toggle heart
-                holder.ivFavsId.setImageResource(isHeartChecked ? R.drawable.ic_heart_uncolored : R.drawable.ic_heart_colored);
-
-                // TODO: add or remove from favorite places
-
-                //set isHeartChecked to !isHeartChecked
-                currShop.setHeartChecked(!isHeartChecked);
-            }
-        });
-
-        // TODO: set ivInfoBtnId onclick handler
-        holder.ivInfoBtnId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // see detailed view of shop
-            }
-        });
-
-        // set cvCardViewId onclick handler
-        holder.cvCardViewId.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // open ShopDetails Activity
-                Intent i = new Intent(context, ShopDetailsActivity.class);
-                i.putExtra("currShop", (Serializable) currShop);
-                context.startActivity(i);
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return shopsList.size();
-    }
-
-
-    public static class ViewHolderOfMapsRecyclerView extends RecyclerView.ViewHolder {
-        private CardView cvCardViewId;
-        private ImageView ivFavsId, ivInfoBtnId;
-        private TextView tvTypeId, tvAddressId;
-
-
-        private ViewHolderOfMapsRecyclerView(@NonNull View itemView) {
-            super(itemView);
-
-            //link Views
-            cvCardViewId = itemView.findViewById(R.id.cvCardViewId);
-            ivFavsId = itemView.findViewById(R.id.ivFavsId);
-            tvTypeId = itemView.findViewById(R.id.tvTypeId);
-            tvAddressId = itemView.findViewById(R.id.tvAddressId);
-            ivInfoBtnId = itemView.findViewById(R.id.ivInfoBtnId);
-        }
-    }
+  }
 }
